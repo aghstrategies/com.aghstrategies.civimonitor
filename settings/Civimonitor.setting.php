1b011e23d4a4afdbfdc4c9b51affc8b3ae982651
Andrew Hunt <andrew@aghstrategies.com>
2016-04-25 14:41:52 -0400

    Never declared the settings before

---
+++ settings/Civimonitor.setting.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Settings metadata for com.aghstrategies.eventpermissions.
+ */
+
+return array(
+  'lastCron' => array(
+    'group_name' => 'CiviMonitor',
+    'group' => 'Civimonitor',
+    'name' => 'lastCron',
+    'type' => 'Integer',
+    'default' => NULL,
+    'add' => '4.6',
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'description' => 'Timestamp of last cron run',
+    'help_text' => 'Set when cron is run',
+  ),
+);
